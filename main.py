c2c6653303b9c57a88b5d51bf47d2fa5812bd1f4
hmunhoz <henrique.boix@gmail.com>
2023-03-07 10:49:39 +0100

    Fix: Typo in main execution

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-import openai
 import warnings
 
+import openai
 from rich import print
 from rich.markdown import Markdown
 from termcolor import colored
 
 warnings.filterwarnings("ignore")
 
-def get_openai_response(messages):
 
+def get_openai_response(messages):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
         temperature=0.7,
     )
 
-    return response.choices[0]['message']
+    return response.choices[0]["message"]
+
 
 def main():
-
     messages = []
 
     while True:
-
         prompt = input(colored("You: ", "green"))
-        
+
         if prompt.lower() in ["exit", "quit"]:
             break
 
         messages.append({"role": "user", "content": prompt})
-        
+
         response = get_openai_response(messages)
         messages.append(dict(response))
 
-        print('\n')
-        print(Markdown(response['content']))
-        print('\n')
+        print("\n")
+        print(Markdown(response["content"]))
+        print("\n")
+
 
 if __name__ == "__main__":
-    main()it
+    main()
